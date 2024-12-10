11a1574fde49510e518536c6e332a07d623523ba
Sagar R <2109417@reva.edu.in>
2024-12-10 13:03:37 +0530

    Create ApprovedCc.tsx

---
+++ assets/assets/ComponentsUserComponents/ApprovedCc.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+const ApprovedCc: React.FC = () => {
+  return (
+    <div className="approvedcc-container">
+      <h1>Admin Approved Content Creators List</h1>
+      <p>Consists of the content creators approved by the admin.</p>
+    </div>
+  );
+};
+
+export default ApprovedCc;
